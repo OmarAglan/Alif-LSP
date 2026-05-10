5febda1d6831bb124aa2d4ef76a1a2700bcfcc02
Omar Aglan <omar.aglan91@yahoo.com>
2026-05-11 00:27:03 +0300

    revert: remove Arabic letters from triggerCharacters
    - Adding too many multi-byte trigger characters breaks some LSP clients (like nvim-cmp)
    
    - Restored space and dot as the primary trigger characters

--- src/Server.cpp
+++ src/Server.cpp
@@ -72,11 +72,8 @@ void LSPServer::handleInitialize(const json& params, const json& id) {
 
 	json capabilities = {
 		{"completionProvider", {
-			{"triggerCharacters", json::array({
-				".", 
-				"ا", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ", "ر", "ز", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "ه", "و", "ي",
-				"أ", "إ", "آ", "ة", "ى", "ئ", "ء", "ؤ"
-			})} 
+			// triggerCharacters MUST be an array of strings
+			{"triggerCharacters", json::array({".", " "})} 
 		}},
 		{"textDocumentSync", 1} // Full sync
 	};
