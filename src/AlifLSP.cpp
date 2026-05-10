0a33e0b5b57093810178fc08b57ef5abbbb0e5e1
Omar Aglan <omar.aglan91@yahoo.com>
2026-05-11 00:43:26 +0300

    feat: add command line flags for logging
    - Support --debug or -v arguments to dynamically enable DEBUG log level without recompiling

--- src/AlifLSP.cpp
+++ src/AlifLSP.cpp
@@ -1,10 +1,21 @@
 #include "Server.h"
 #include "Transport.h"
+#include "Logger.h"
 
 #include <iostream>
 #include <memory>
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
+	// Parse command line arguments
+	for (int i = 1; i < argc; ++i) {
+		std::string arg = argv[i];
+		if (arg == "--debug" || arg == "-v") {
+			Logger::setLevel(LogLevel::DEBUG);
+			Logger::debug("Debug logging enabled via command line");
+		}
+	}
+
 	// Disable stdio buffering
 	setvbuf(stdin, nullptr, _IONBF, 0);
 	setvbuf(stdout, nullptr, _IONBF, 0);
